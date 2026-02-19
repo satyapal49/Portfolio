8e76eb701f73015c5eccb7d276cdd6c49ce69d66
Satyapal Raj <satyapalraj@gmail.com>
2026-02-19 13:00:50 +0530

    updated link

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProjectCard = ({title, description, image, tech})=>{
+const ProjectCard = ({title, description, image, tech, demo, code})=>{
     return (
         <div className="bg-dark-300 rounded-2xl overflow-hidden hover:-translate-y-2 transition duration-300 cursor-pointer">
             <img src={image} alt={title} className="w-full h-60 object-cover" />
@@ -15,10 +15,10 @@ const ProjectCard = ({title, description, image, tech})=>{
                     ))}
                 </div>
                 <div className="flex gap-2">
-                    <a href="#" className="flex-1 text-center px-4 py-2 bg-orange rounded-lg font-medium hover:bg-orange-700 transition duration-300">
+                    <a href={demo} className="flex-1 text-center px-4 py-2 bg-orange rounded-lg font-medium hover:bg-orange-700 transition duration-300">
                         View Demo
                     </a>
-                    <a href="#" className="flex-1 text-center px-4 py-2 border border-orange font-medium rounded-lg gover:bg-orange/20 transition duration-300">Get Code</a>
+                    <a href={code} className="flex-1 text-center px-4 py-2 border border-orange font-medium rounded-lg gover:bg-orange/20 transition duration-300">Get Code</a>
                 </div>
             </div>
         </div>
